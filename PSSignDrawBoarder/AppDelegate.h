eea0497b28d25a06aa96923095fb2d0de570bc9f
PS <psxing779@163.com>
2017-11-25 13:16:09 +0800

    Initial Commit

---
+++ PSSignDrawBoarder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSSignDrawBoarder
+//
+//  Created by Vic on 2017/11/25.
+//  Copyright © 2017年 Vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PSSignDrawBoarder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PSSignDrawBoarder
+//
+//  Created by Vic on 2017/11/25.
+//  Copyright © 2017年 Vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
